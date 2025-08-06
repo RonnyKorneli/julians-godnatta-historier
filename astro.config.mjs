ee1c1b33f4789b4eec0af1cab52e9a6ae3350708
Ronny Korneli <>
2025-08-06 12:28:30 +0200

    ran sass migration command

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,30 @@
-// https://docs.astro.build/de/reference/configuration-reference/
-
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import mkcert from 'vite-plugin-mkcert';
 
-
-
 export default defineConfig({
+  output: 'server', // ✅ needed for middleware
 
   i18n: {
     locales: ["no", "en", "de"],
     defaultLocale: "no",
     routing: {
-        prefixDefaultLocale: true
-    }},
+      prefixDefaultLocale: true,
+    },
+  },
 
-
-  output: 'static',
   compressHTML: true,
   trailingSlash: 'always',
-  base: (process.env.npm_lifecycle_event === 'stage') ? 'astro-starter/' : '/',
+  base: '/',
   vite: {
-    plugins: [
-      mkcert(),
-    ],
+    plugins: [mkcert()],
   },
   server: {
     host: true,
-    https: true
+    https: true,
   },
   build: {
-    sourcemap: (process.env.npm_lifecycle_event === 'dev') ? true : false,
+    sourcemap: process.env.npm_lifecycle_event === 'dev',
   },
   integrations: [sitemap()],
 });
